b710543e8d19e5e0d6f6fe01754128a5cb342bf6
paris osuch <paris.osuch@gmail.com>
2025-09-07 13:56:03 -0700

    feat: create no auth view

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
 import { ArrowRight, LandmarkIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Home() {
+import { createClient } from "@/lib/supabase/server";
+
+const NoAuthView = () => (
+  <div className="flex flex-col items-center">
+    <div className="flex flex-row items-center text-4xl font-semibold space-x-2">
+      <LandmarkIcon size={48} />
+      <h1>Welcome to Colosseum.</h1>
+    </div>
+    <p className="text-muted-foreground">
+      Account creation is currently invite only.
+    </p>
+    <Link
+      href="/auth/login"
+      className="flex flex-row items-center justify-center mt-4 space-x-1"
+    >
+      <p className="underline">Login</p>
+      <ArrowRight size={16} />
+    </Link>
+  </div>
+);
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.auth.getUser();
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex flex-row items-center text-4xl font-semibold space-x-2">
-        <LandmarkIcon size={48} />
-        <h1>Welcome to Colosseum.</h1>
-      </div>
-      <p className="text-muted-foreground">Account creation is currently invite only.</p>
-      <Link href='/auth/login' className="flex flex-row items-center justify-center mt-4 space-x-1">
-        <p className="underline">Login</p>
-        <ArrowRight size={16} />
-      </Link>
-    </main >
+      {!data.user ? <NoAuthView /> : null}
+    </main>
   );
 }

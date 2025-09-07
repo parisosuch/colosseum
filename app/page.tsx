a892065f4df2c0c810cdbc5b8617569baba5e41b
Paris Osuch <paris.osuch@gmail.com>
2025-09-06 19:16:14 -0700

    starter landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
+import { ArrowRight, LandmarkIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-    </main>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-row items-center text-4xl font-semibold space-x-2">
+        <LandmarkIcon size={48} />
+        <h1>Welcome to Colosseum.</h1>
+      </div>
+      <p>Account creation is currently invite only.</p>
+      <Link href='/auth/login' className="flex flex-row items-center justify-center mt-4 space-x-1">
+        <p className="underline">Login</p>
+        <ArrowRight size={16} />
+      </Link>
+    </main >
   );
 }

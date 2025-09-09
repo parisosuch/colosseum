c00ac077dbaafbf029905f204d6647df52afe0df
paris osuch <paris.osuch@gmail.com>
2025-09-08 17:45:09 -0700

    redirect to user profile if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { ArrowRight, LandmarkIcon } from "lucide-react";
 import Link from "next/link";
 
 import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { getUserProfile } from "@/lib/colosseum/user";
 
 const NoAuthView = () => (
   <div className="flex flex-col items-center">
@@ -29,9 +31,32 @@ export default async function Home() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      {!user ? <NoAuthView /> : null}
-    </main>
-  );
+  // if there is no user, immediately show the sign-in view
+
+  if (!user) {
+    return (
+      <main className="min-h-screen flex flex-col items-center justify-center">
+        <div className="flex flex-row items-center text-4xl font-semibold space-x-2">
+          <LandmarkIcon size={48} />
+          <h1>Welcome to Colosseum.</h1>
+        </div>
+        <p className="text-muted-foreground">
+          Account creation is currently invite only.
+        </p>
+        <Link
+          href="/auth/login"
+          className="flex flex-row items-center justify-center mt-4 space-x-1"
+        >
+          <p className="underline">Login</p>
+          <ArrowRight size={16} />
+        </Link>
+      </main>
+    );
+  }
+
+  // get the user handle and redirect to the user's profile
+
+  const userProfile = await getUserProfile(supabase, user.id);
+
+  redirect(`/${userProfile?.handle}`);
 }

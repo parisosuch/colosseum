93748fe78628f99988b3df10206401c256ef5217
paris osuch <paris.osuch@gmail.com>
2025-09-08 20:07:27 -0700

    Create column type

---
+++ lib/colosseum/column.ts
@@ -0,0 +1,12 @@
+export type Column = {
+  id: number;
+  created_at: string;
+  type: string;
+  title: string;
+  description?: string;
+  urL?: string;
+  text?: string;
+  image?: string;
+  created_by: string;
+  channel_id: string;
+};

221a9306413e64bdd5a600e5ca20c0c4845139d6
paris osuch <paris.osuch@gmail.com>
2025-09-08 17:32:35 -0700

    get user profile from protected page as an example

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,14 +3,20 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
+import { getUserProfile } from "@/lib/colosseum/user";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
     redirect("/auth/login");
   }
+
+  const userProfile = await getUserProfile(supabase, user.id);
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
@@ -24,7 +30,11 @@ export default async function ProtectedPage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
+          {JSON.stringify(user)}
+        </pre>
+        <h2 className="font-bold text-2xl mb-4">Your user profile</h2>
+        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
+          {JSON.stringify(userProfile)}
         </pre>
       </div>
       <div>

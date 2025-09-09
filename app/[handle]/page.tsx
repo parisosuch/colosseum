83573f4d3d7c8239e7a9ed2b4aafa54c4ffe9a99
paris osuch <paris.osuch@gmail.com>
2025-09-08 18:02:14 -0700

    check if user exist

--- app/[handle]/page.tsx
+++ app/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { getUserProfile, UserProfile } from "@/lib/colosseum/user";
 import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 type PageProps = {
   params: { handle: string };
@@ -15,13 +16,15 @@ export default async function UserPage({ params }: PageProps) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  console.log(user);
-
   const { data, error } = await supabase
     .from("user_profile")
     .select("user_id")
     .eq("handle", handle)
     .single();
+
+  if (!data) {
+    return <div>This user does not exist.</div>;
+  }
 
   if (!user) {
     return <div>Not authenticated</div>;

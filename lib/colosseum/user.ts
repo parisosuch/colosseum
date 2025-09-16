38b36912c57a8ce24d2f13ef1adaa2a132519d0a
paris osuch <paris.osuch@gmail.com>
2025-09-15 21:10:24 -0700

    add function that gets the public user profile

--- lib/colosseum/user.ts
+++ lib/colosseum/user.ts
@@ -8,6 +8,27 @@ export type UserProfile = {
   about?: string;
 };
 
+export async function getPublicUserProfile(
+  client: SupabaseClient,
+  handle: string
+): Promise<UserProfile> {
+  const { data, error } = await client
+    .from("user_profile")
+    .select("*")
+    .eq("handle", handle)
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data) {
+    throw new Error(`User with handle ${handle} does not exist.`);
+  }
+
+  return data;
+}
+
 export async function getUserProfile(
   client: SupabaseClient,
   user_id: string

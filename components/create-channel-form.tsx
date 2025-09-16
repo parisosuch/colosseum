f7731898013e9806ddc370450cbda3e19e7dcedf
Paris Osuch <paris.osuch@gmail.com>
2025-09-15 20:11:44 -0700

    change checkbox

--- components/create-channel-form.tsx
+++ components/create-channel-form.tsx
@@ -14,16 +14,44 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { PlusIcon } from "lucide-react";
+import { createClient } from "@/lib/supabase/client";
+import { createChannel } from "@/lib/colosseum/channel";
+import { Checkbox } from "./ui/checkbox";
+import { getUserProfile } from "@/lib/colosseum/user";
 
 export default function CreateChannelForm({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [isPrivate, setPrivate] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent) => {
-        // 
+        e.preventDefault();
+        const supabase = createClient();
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            // get the user
+            const {
+                data: { user },
+            } = await supabase.auth.getUser();
+            if (!user) {
+                throw new Error("User not logged in.");
+            }
+
+            // create the channel
+            const channel = await createChannel(supabase, { title: title, description: description, private: isPrivate, owner_id: user.id });
+            // reroute to channel that was just created
+            const userProfile = await getUserProfile(supabase, user.id);
+            router.push(`/${userProfile.handle}/${channel.id}`)
+        } catch (error: unknown) {
+            setError(error instanceof Error ? error.message : "An error occurred");
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -54,6 +82,12 @@ export default function CreateChannelForm({ className, ...props }: React.Compone
                                     type="text"
                                     value={description}
                                     onChange={(e) => setDescription(e.target.value)}
+                                />
+                                <Label htmlFor="text">Private Channel</Label>
+                                <Checkbox
+                                    id="private"
+                                    checked={false}
+                                    onCheckedChange={(state) => setPrivate(state === true)}
                                 />
                             </div>
                             {error && <p className="text-sm text-red-500">{error}</p>}

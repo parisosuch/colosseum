c1000f23828465b67438b572b3ff98531f57708f
Paris Osuch <paris.osuch@gmail.com>
2025-09-15 19:34:04 -0700

    template page

--- app/create-channel/page.tsx
+++ app/create-channel/page.tsx
@@ -1,5 +1,11 @@
+import CreateChannelForm from "@/components/create-channel-form";
+
 export default function CreateChannelPage() {
-    return (<div>
-        This is the create channel page.
-    </div>)
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <CreateChannelForm />
+            </div>
+        </div>
+    );
 }

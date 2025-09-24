6c8564dc63127f342a025f4f6a9b7fea3d174a56
paris osuch <paris.osuch@gmail.com>
2025-09-24 11:17:16 -0700

    Create function that uploads text column

--- lib/colosseum/column.ts
+++ lib/colosseum/column.ts
@@ -1,9 +1,10 @@
 import { SupabaseClient } from "@supabase/supabase-js";
+import { isURL } from "../utils";
 
 export type Column = {
   id: number;
   created_at: string;
-  type: string;
+  type: "url" | "text" | "image";
   title?: string;
   description?: string;
   url?: string;
@@ -15,13 +16,52 @@ export type Column = {
 
 export async function getChannelColumns(
   supabase: SupabaseClient,
-  channel_id: number,
+  channel_id: number
 ): Promise<Column[]> {
   const { data, error } = await supabase
     .from("column")
     .select("*")
     .eq("channel_id", channel_id)
     .order("created_at");
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function uploadTextColumn(
+  supabase: SupabaseClient,
+  column: {
+    created_by: string;
+    channel_id: number;
+    text: string;
+  }
+): Promise<Column> {
+  let columnData;
+
+  if (isURL(column.text)) {
+    columnData = {
+      type: "url",
+      url: column.text,
+      created_by: column.created_by,
+      channel_id: column.channel_id,
+    };
+  } else {
+    columnData = {
+      type: "text",
+      url: column.text,
+      created_by: column.created_by,
+      channel_id: column.channel_id,
+    };
+  }
+
+  const { data, error } = await supabase
+    .from("column")
+    .upsert(columnData)
+    .select()
+    .single();
 
   if (error) {
     throw new Error(error.message);

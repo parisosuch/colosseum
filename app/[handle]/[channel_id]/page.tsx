fa6975aa0e483dadc1aad675758a3b519d7cccd1
Paris Osuch <paris.osuch@gmail.com>
2025-10-12 08:58:00 -0700

    Apparently idk how to style

--- app/[handle]/[channel_id]/page.tsx
+++ app/[handle]/[channel_id]/page.tsx
@@ -145,12 +145,7 @@ export default function ChannelPage() {
       </div>
       <div
         className="grid gap-4 
-                grid-cols-1
-                sm:grid-cols-2
-                md:grid-cols-3
-                lg:grid-cols-4
-                xl:grid-cols-5
-                2xl:grid-cols-6
+                grid-cols-5
                 3xl:grid-cols-7"
       >
         <ColumnInput

5d34353f84d781a1d1af9eae704d7c23dbef4d8b
paris osuch <paris.osuch@gmail.com>
2025-10-18 18:36:19 -0700

    Remove unused component

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,6 @@ import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import { getUserProfile } from "@/lib/colosseum/user";
 
-const NoAuthView = () => (
-  <div className="flex flex-col items-center">
-    <div className="flex flex-row items-center text-4xl font-semibold space-x-2">
-      <LandmarkIcon size={48} />
-      <h1>Welcome to Colosseum.</h1>
-    </div>
-    <p className="text-muted-foreground">
-      Account creation is currently invite only.
-    </p>
-    <Link
-      href="/auth/login"
-      className="flex flex-row items-center justify-center mt-4 space-x-1"
-    >
-      <p className="underline">Login</p>
-      <ArrowRight size={16} />
-    </Link>
-  </div>
-);
-
 export default async function Home() {
   const supabase = await createClient();
 
